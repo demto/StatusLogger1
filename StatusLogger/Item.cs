378f1f0e419c8ba730d31f0ccf13895fd47e1bce
Tom <demto13@gmail.com>
2018-03-31 20:44:24 +0100

    Added Item and event handling

---
+++ StatusLogger/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusLogger {
+    class Item {
+
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public EventHandler<EventArgs> ItemStatusChanged;
+        private ItemConstants itemStatus;
+        public ItemConstants ItemStatus { 
+            get { return itemStatus; } 
+            set {
+                if(value != itemStatus){
+                    itemStatus = value;
+                    OnItemStatusChanged(this.ItemName, this.ItemStatus);
+                }
+            }
+        }
+
+        protected void OnItemStatusChanged(string itemName, ItemConstants itemStatus){
+            ItemStatusChanged?.Invoke(this, new EventArgs());
+    }
+}
