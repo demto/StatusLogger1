c10f114c7fbcd2a7ec698fa13dfefb7e50b65299
Tom <demto13@gmail.com>
2018-03-31 20:48:09 +0100

    Added new type of event

---
+++ StatusLogger/ItemStatusEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusLogger {
+    class ItemStatusEventArgs : EventArgs{
+
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public ItemConstants ItemStatus { get; set; }
+
+        public ItemStatusEventArgs(string itemName, ItemConstants itemStatus) {
+            this.ItemName = itemName;
+            this.ItemStatus = itemStatus;
+        }
+}
