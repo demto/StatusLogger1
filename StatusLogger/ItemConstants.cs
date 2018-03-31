098997aad96e6752e7076b57202bd1c937ffcd4d
Tom <demto13@gmail.com>
2018-03-31 20:31:49 +0100

    Added enum and logger

---
+++ StatusLogger/ItemConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusLogger {
+    class ItemConstants {
+
+        public enum Statuses{
+            OutOfStock,
+            InStock,
+            SoldOut,
+            Discounted
+        }
+    }
+}
